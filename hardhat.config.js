54932ec4c25099d248316742e7dee12a4394886f
de33 <davideiber@gmail.com>
2022-02-18 13:46:41 -0700

    Add initial sweep-dust-script

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,11 @@ require('dotenv').config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.4",
   networks: {
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
-      accounts: [process.env.PRIVATE_KEY],
+    hardhat: {
+      chainId: 1
     },
   },
 };
--- scripts/sweep-dust-script.js
+++ scripts/sweep-dust-script.js
@@ -1,28 +1,85 @@
 const ethers = require("ethers");
 const { getDefaultProvider } = require("@ethersproject/providers");
 const { Wallet } = require("@ethersproject/wallet");
-const {ContractFactory} = require("@e")
 const {
   abi: dustsweeperABI,
-  bytecode: dustsweeperBytecode
+  bytecode: dustsweeperBytecode,
 } = require("../artifacts/contracts/DustSweeper.sol/DustSweeper.json");
+const {
+  abi: erc20ABI,
+} = require("../artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json");
+const { tokens } = require("../tokens.json");
+const { get, gt } = require("lodash");
 const dustsweeperInterface = new ethers.utils.Interface(dustsweeperABI);
+const { getTokenList } = require("../utils");
+
 require("dotenv").config();
 
 const PK = process.env.PRIVATE_KEY;
-const ALCHEMY_FORK = process.env.ALCHEMY_FORK;
-const dustsweeperAddress;
+const dustsweeperAddress = 0x5fbdb2315678afecb367f032d93f642f64180aa3;
 
 async function main() {
-    const provider = getDefaultProvider(ALCHEMY_FORK);
+  const provider = getDefaultProvider("http://127.0.0.1:8545/");
 
-    const signer = new Wallet(PK, provider);
+  console.log(await provider.getNetwork())
 
-    const dustSweeperContract = new ethers.ContractFactory( dustsweeperInterface , dustsweeperBytecode  , signer )
+  // const signer = new Wallet(PK, provider);
 
-    //const dustSweeperDeployed = dustSweeperContract.attach(dustsweeperAddress)
+  // const dustSweeperContract = new ethers.ContractFactory(
+  //   dustsweeperInterface,
+  //   dustsweeperBytecode,
+  //   signer
+  // );
 
-    //const sweepTx = dustSweeperDeployed.sweepDust()
+  // const dustSweeperDeployed = dustSweeperContract.attach(dustsweeperAddress);
+  // const sweepTx = dustSweeperDeployed.sweepDust();
+
+  // const tokenAddressArray = getTokenList();
+
+  // let tmpApprovalList = [];
+  // for (let i = 0; i < tokenAddressArray.length; i++) {
+  //   const erc20Contract = new ethers.Contract(
+  //     tokenAddressArray[i],
+  //     erc20ABI,
+  //     provider
+  //   );
+
+  //   const filterSpender = erc20Contract.filters.Approval(
+  //     null,
+  //     dustsweeperAddress
+  //   );
+  //   const logsFrom = await erc20Contract.queryFilter(
+  //     filterSpender,
+  //     -5000,
+  //     "latest"
+  //   );
+
+  //   for (let j = 0; j < logsFrom.length; j++) {
+  //     const owner = get(logsFrom[j], "args.owner");
+  //     const spender = get(logsFrom[j], "args.spender");
+
+  //     try {
+  //       const allowance = await erc20Contract.allowance(owner, spender);
+
+  //       if (allowance.gt("0")) {
+  //         let approval = logsFrom[j];
+  //         const symbol = await erc20.symbol();
+  //         const decimals = await erc20.decimals();
+
+  //         console.log(allowance);
+  //         console.log(decimals);
+  //         approval.owner = owner;
+  //         approval.spender = spender;
+  //         approval.symbol = symbol;
+  //         approval.allowance = allowance;
+  //         approval.decimals = decimals;
+  //         tmpApprovalList.push(approval);
+  //       }
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   }
+  // }
 }
 
 main()
